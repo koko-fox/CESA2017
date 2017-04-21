a85c9deba10672503ce7b8946c48a5e436ba64ff
unknown <s152106>
2017-04-21 10:01:00 +0900

    インスペクタ拡張
    追加
    -ゲージUIのインスペクタ拡張

--- Assets/Shibata/Script/UIGuageController.cs
+++ Assets/Shibata/Script/UIGuageController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class UIGuageController : MonoBehaviour
 {
 	[SerializeField]
-	[Header("ゲージの画像")]
 	private Image _guage;
+	public Image Guage
+	{
+		get { return _guage; }
+		set
+		{
+			_guage = value;
+		}
+	}
 
 	[SerializeField]
 	[Header("ゲージ背景の画像")]
@@ -17,6 +28,53 @@ public class UIGuageController : MonoBehaviour
 	[SerializeField]
 	[Header("ゲージの長さ")]
 	private float _length=1.0f;
+	public float Length
+	{
+		get { return _length; }
+		set
+		{
+			_length = value;
+			_guage.rectTransform.localScale = new Vector3(_length, 1.0f, 1.0f);
+			_guageBack.rectTransform.localScale = new Vector3(_length, 1.0f, 1.0f);
+		}
+	}
+
+	[SerializeField]
+	private Color _frontColor = Color.white;
+	public Color FrontColor
+	{
+		get { return _frontColor; }
+		set
+		{
+			_frontColor = value;
+			_guage.color = _frontColor;
+		}
+	}
+
+	[SerializeField]
+	private Color _backColor = Color.white;
+	public Color BackColor
+	{
+		get { return _backColor; }
+		set
+		{
+			_backColor = value;
+			_guageBack.color = _backColor;
+		}
+	}
+
+	[SerializeField]
+	private Color _iconColor = Color.white;
+	public Color IconColor
+	{
+		get { return _iconColor; }
+		set
+		{
+			_iconColor = value;
+			
+		}
+	}
+
 
 	[SerializeField]
 	[Header("ゲージの変形速度")]
@@ -47,6 +105,9 @@ public class UIGuageController : MonoBehaviour
 
 	private void Awake()
 	{
+		_guage = transform.FindChild("Guage").GetComponent<Image>();
+		_guageBack = transform.FindChild("GuageBack").GetComponent<Image>();
+
 		_guage.rectTransform.localScale = new Vector3(0.0f, 1.0f, 1.0f);
 		_guageBack.rectTransform.localScale = new Vector3(0.0f, 1.0f, 1.0f);
 		_guage.rectTransform.DOScaleX(_length, _guageScaleDuration);
@@ -61,4 +122,31 @@ public class UIGuageController : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.M))
 			Rate -= 0.1f;
 	}
+
+#if UNITY_EDITOR
+
+	[CustomEditor(typeof(UIGuageController))]
+	[CanEditMultipleObjects]
+	public class UIGuageControllerEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			UIGuageController cont = target as UIGuageController;
+
+			var length = EditorGUILayout.FloatField("ゲージの長さ", cont.Length);
+			var frontColor = EditorGUILayout.ColorField("フロントカラー", cont.FrontColor);
+			var backColor = EditorGUILayout.ColorField("バックカラー", cont.BackColor);
+
+			Undo.RecordObject(cont, "UIGuageController Changed");
+			cont.Length = length;
+			cont.FrontColor = frontColor;
+			cont.BackColor = backColor;
+
+			EditorUtility.SetDirty(cont);
+		}
+	}
+
+#endif
 }
