610e982bc2baaa1bc7a092c14883277db3399577
RyutaroSUZUKI <szkrtr1992@yahoo.co.jp>
2017-05-30 14:27:17 +0900

    #28 自爆する敵が破壊される際にプレイヤーや盾がダメージを受ける

--- Assets/Suzuki/Scripts/Enemy/EnemyDetonator.cs
+++ Assets/Suzuki/Scripts/Enemy/EnemyDetonator.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class EnemyDetonator : EnemyModuleBase {
   private KamikazeEnemyController controller;
 
+  [SerializeField]
+  private float explosionRadius;
+  [SerializeField]
+  private float firePower;
+  [SerializeField]
+  private LayerMask targetLayer;
+
   protected override void DoAwake() {
     controller = GetComponent<KamikazeEnemyController>();
   }
@@ -12,6 +19,26 @@ public class EnemyDetonator : EnemyModuleBase {
   private void OnCollisionEnter(Collision collision) {
     if (IsEnemy(collision.gameObject)) return;
     if (!controller.isFlying) return;
+
+    // 爆風発生
+    var position = transform.position;
+    var radius = explosionRadius;
+    var layerMask = targetLayer;
+
+    Collider[] colliders = Physics.OverlapSphere(position, radius, layerMask);
+    foreach (var elem in colliders) {
+      var layer = LayerMask.NameToLayer("RadiateShield");
+      if (elem.gameObject.layer == layer) {
+        var shield = elem.gameObject.GetComponent<ShieldCore>();
+        shield.responseSystem.NoticeHitBullet(collision.contacts[0].point);
+      }
+      layer = LayerMask.NameToLayer("UnityChan");
+      if (elem.gameObject.layer == layer) {
+        var player = elem.gameObject.GetComponent<ChanCore>();
+        player.healthSystem.health -= firePower;
+      }
+    }
+
     core.Die();
   }
 
