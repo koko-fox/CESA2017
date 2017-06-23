868a058845cfb15d11edf7651bc49761dc30b002
shibata mizuki <shibata.public@gmail.com>
2017-06-23 10:30:23 +0900

    カーソルの状態を外部から操作できるように TFCursor.Enable()...カーソル有効化 TFCursor.Disable()...カーソル無効化

--- Assets/Shibata/Systems/CursorOperationModeChanger/Scripts/CursorOperationModeChanger.cs
+++ Assets/Shibata/Systems/CursorOperationModeChanger/Scripts/CursorOperationModeChanger.cs
@@ -3,58 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.ComponentModel;
 
-public class CursorOperationModeChanger : Lockable
+public static class TFCursor
 {
-	bool _isFree = false;
+	#region private fields
+	static bool _initialized = false;
+	static bool _enabled;
+	static ChanCoreSystem _chan;
+	#endregion
 
-	[SerializeField]
-	ChanCoreSystem _chan;
-	[SerializeField]
-	SpecterCore _specter;
-	[SerializeField]
-	ControlModeChanger _controlModeChanger;
-
-	private void Awake()
+	#region properties
+	public static bool enabled
 	{
-		if(!_chan)
-			_chan = FindObjectOfType<ChanCoreSystem>();
-		if(!_specter)
-			_specter = FindObjectOfType<SpecterCore>();
-		if (!_controlModeChanger)
-			_controlModeChanger = FindObjectOfType<ControlModeChanger>();
+		get { return _enabled; }
+	}
+	#endregion
+
+	#region private methods
+	static void Initialize()
+	{
+		_chan = GameObject.FindObjectOfType<ChanCoreSystem>();
+		_initialized = true;
+	}
+	#endregion
+
+	#region public methods
+	/// <summary>カーソルを有効化する</summary>
+	public static void Enable()
+	{
+		if (!_initialized)
+			Initialize();
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+
+		if (_chan)
+		{
+			foreach (var elem in _chan.modules)
+			{
+				elem.locked = true;
+			}
+		}
+		else
+		{
+			TFDebug.Log("@system", "Chanの無効化に失敗\nChanCoreSystemが見つかりませんでした");
+		}
+		_enabled = true;
+	}
+
+	/// <summary>カーソルを無効化する</summary>
+	public static void Disable()
+	{
+		if (!_initialized)
+			Initialize();
 
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
+
+		if (_chan)
+		{
+			foreach (var elem in _chan.modules)
+			{
+				elem.locked = false;
+			}
+		}
+		else
+		{
+			TFDebug.Log("@system", "Chanの有効化に失敗\nChanCoreSystemが見つかりませんでした");
+		}
+		_enabled = false;
+	}
+	#endregion
+}
+
+public class CursorOperationModeChanger : MonoBehaviour
+{
+	#region unity methods
+	void Awake()
+	{
+		TFCursor.Disable();
 	}
 
-	protected override void LockableUpdate()
+	private void Update()
 	{
 		if(Input.GetKeyDown(KeyCode.LeftAlt))
 		{
-			_isFree = !_isFree;
-
-			if(_isFree)
+			if(TFCursor.enabled)
 			{
-				Cursor.lockState = CursorLockMode.None;
-				Cursor.visible = true;
-
-				foreach (var elem in _chan.modules)
-					elem.locked = true;
-				_specter.LockAll();
-				_controlModeChanger.isLock = true;
+				TFCursor.Disable();
 			}
 			else
 			{
-				Cursor.lockState = CursorLockMode.Locked;
-				Cursor.visible = false;
-
-				foreach (var elem in _chan.modules)
-					elem.locked = false;
-				_specter.UnlockAll();
-
-				_controlModeChanger.isLock = false;
-				_controlModeChanger.Change(_controlModeChanger.isChan);
+				TFCursor.Enable();
 			}
 		}
 	}
+	#endregion
 }
